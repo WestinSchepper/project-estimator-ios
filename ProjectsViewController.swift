4af81c1840ac650520720b8d0870a19418831a3b
Westin Schepper <westin@teeps.org>
2017-11-04 19:20:50 -0400

    Add projects view controller

---
+++ ProjectsViewController.swift
@@ -0,0 +1,9 @@
+//  Created by Westin Schepper on 11/4/17.
+
+import UIKit
+
+final class ProjectsViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
