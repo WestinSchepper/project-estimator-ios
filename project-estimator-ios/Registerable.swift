ff93d84f0e7936e0bc0012fa636b26eb0531440a
Westin Schepper <westin@teeps.org>
2017-11-05 00:10:52 -0400

    Update Registerable protocol

---
+++ project-estimator-ios/Registerable.swift
@@ -0,0 +1,42 @@
+//  Created by Westin Schepper on 9/4/17.
+
+import UIKit
+
+protocol Registerable {
+  static var identifier: String { get }
+  static var nib: UINib { get }
+}
+
+extension Registerable {
+  static var identifier: String {
+    return String(describing: Self.self)
+  }
+
+  static var nib: UINib {
+    return UINib(nibName: identifier, bundle: nil)
+  }
+}
+
+extension Registerable where Self: UITableViewCell {
+  static func register(tableView: UITableView) {
+    tableView.register(nib, forCellReuseIdentifier: identifier)
+  }
+
+  static func cell(forTable table: UITableView) -> Self {
+    let cell = table.dequeueReusableCell(withIdentifier: identifier) as! Self
+
+    return cell
+  }
+}
+
+extension Registerable where Self: UICollectionViewCell {
+  static func register(collectionView: UICollectionView) {
+    collectionView.register(nib, forCellWithReuseIdentifier: identifier)
+  }
+
+  static func cell(forCollection collection: UICollectionView, withIndexPath indexPath: IndexPath) -> Self {
+    let cell = collection.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! Self
+
+    return cell
+  }
+}
