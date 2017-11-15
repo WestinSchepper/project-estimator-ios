11bb45117223e57d4e6ae3c6c372d12eb7461a4e
Westin Schepper <westin@teeps.org>
2017-11-14 23:38:59 -0500

    Add project detail ui

---
+++ project-estimator-ios/ProjectViewController.swift
@@ -0,0 +1,27 @@
+//  Created by Westin Schepper on 11/14/17.
+
+import UIKit
+
+final class ProjectViewController: UIViewController {
+  @IBOutlet weak var headerContainer: UIView!
+  @IBOutlet weak var projectTitle: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var hoursLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+
+  @IBAction func close(_ sender: UIButton) {
+
+  }
+
+  @IBAction func more(_ sender: UIButton) {
+
+  }
+
+  @IBAction func expand(_ sender: UIButton) {
+
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
