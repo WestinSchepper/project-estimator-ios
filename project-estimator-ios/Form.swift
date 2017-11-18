68575a573cc4b43e4be5fe877c6253a0c82b7ce3
Westin Schepper <westin@teeps.org>
2017-11-18 13:47:22 -0500

    Add and implement first responder functionality to the forms

--- project-estimator-ios/Form.swift
+++ project-estimator-ios/Form.swift
@@ -1,10 +1,18 @@
 //  Created by Westin Schepper on 11/11/17.
 
+import Foundation
+
 class Form {
   var title: String
   var fields: [FormField] = []
   var valid: Bool {
     return fields.contains(where: { $0.valid == false }) == false
+  }
+
+  var firstResponderIndex: IndexPath? {
+    guard let index = fields.index(where: { $0.shouldBecomeFirstResponder }) else { return nil }
+
+    return IndexPath(row: index, section: 0)
   }
 
   lazy var viewController: FormViewController = {
